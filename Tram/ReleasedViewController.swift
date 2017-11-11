d795e208d830ebd735efee2aac12e2b99b88e44b
romikabi <abuzyarovroman@gmail.com>
2017-11-11 18:44:55 +0300

    Application's skeleton.

---
+++ Tram/ReleasedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ReleasedViewController.swift
+//  Tram
+//
+//  Created by Roman Abuzyarov on 11.11.2017.
+//  Copyright © 2017 Tram, inc. All rights reserved.
+//
+
+import UIKit
+
+
+class ReleasedViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
